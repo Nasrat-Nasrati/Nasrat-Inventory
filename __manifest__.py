b207d77b2cdf0a7d83dcbc78b7377f8e3b7b72f5
Nasrat-Nasrati <ehnn2020@gmail.com>
2024-07-09 08:46:19 +0430

    this is just for inventory customization with odoo17 by Nasrat Nasrati

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# __manifest__.py
+
+{
+    'name': 'Nasrat Inventory',
+    'version': '17.0.0.0.1',
+    'category': 'inventory  Customization',
+    'summary': 'this is just for whole inventory',
+    'sequence': -3,
+    'description': """
+      this is belongs to all inventory version 17
+    """,
+    'author': 'Nasrat Nasrati',
+    'website': 'https://www.example.com',
+    'depends': ['stock'],
+    'data': [ ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
